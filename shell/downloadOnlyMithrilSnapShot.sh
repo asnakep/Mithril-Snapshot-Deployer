fedb744eeb43d6e81f800c30869373465d243adb
SNAKE (Cardano Stakepool) <53517506+asnakep@users.noreply.github.com>
2023-11-27 16:28:15 +0100

    Update downloadOnlyMithrilSnapShot.sh

--- shell/downloadOnlyMithrilSnapShot.sh
+++ shell/downloadOnlyMithrilSnapShot.sh
@@ -1,12 +1,12 @@
-#!/run/current-system/sw/bin/bash
+#!/bin/bash
 
-export white=`printf "\033[1;37m"`
-export indi=`printf "\033[1;35m"`
+export whi=`printf "\033[1;37m"`
+export gre=`printf "\033[1;36m"`
 
 tput clear
 
 echo
-echo $indi"Download Latest Mithril Mainnet Snapshot"
+echo $gre"Deploy Latest Mithril Mainnet Snapshot"
 echo
 
 ### snapshots json url
@@ -17,53 +17,78 @@ last_snapshot=$(curl -s $snapshots | jq -r .[0])
 
 # Get Snapshot data
 digest=$(echo $last_snapshot | jq -r '.digest')
-echo $white"Snapshot Digest: $indi$digest"
+echo $whi"Snapshot Digest: $gre$digest"
 
 network=$(echo $last_snapshot | jq -r '.beacon.network')
-echo $white"Network: $indi$network"
+echo $whi"Network: $gre$network"
 
 epoch=$(echo $last_snapshot | jq -r '.beacon.epoch')
-echo $white"Epoch: $indi$epoch"
+echo $whi"Epoch: $gre$epoch"
 
 immutable_file_number=$(echo $last_snapshot | jq -r '.beacon.immutable_file_number')
-echo $white"Immutable File Number: $indi$immutable_file_number"
+echo $whi"Immutable File Number: $gre$immutable_file_number"
 
 certificate_hash=$(echo $last_snapshot | jq -r '.certificate_hash')
-echo $white"Certificate Hash: $indi$certificate_hash"
+echo $whi"Certificate Hash: $gre$certificate_hash"
 
 size=$(echo $last_snapshot | jq -r '.size')
 size_gb=$((size / 1024 / 1024))
 size_gb_format=$(echo $size_gb | awk '{print substr($0, 1, length($0)-3) "." substr($0, length($0)-1)}')
-echo $white"Size: $indi$size_gb_format""Gb"
+echo $whi"Size: $gre$size_gb_format""Gb"
 
 created_at=$(echo $last_snapshot | jq -r '.created_at' | awk '{print substr($0, 1, length($0)-11)}')
-echo $white"Created At: $indi$created_at"
+echo $whi"Created At: $gre$created_at"
 
 compression_algorithm=$(echo $last_snapshot | jq -r '.compression_algorithm')
-echo $white"Compression Algorithm: $indi$compression_algorithm"
+echo $whi"Compression Algorithm: $gre$compression_algorithm"
 
 cardano_node_version=$(echo $last_snapshot | jq -r '.cardano_node_version')
-echo $white"Cardano Node Version: $indi$cardano_node_version"
+echo $whi"Cardano Node Version: $gre$cardano_node_version"
 
 downloadUrl=$(echo $last_snapshot | jq -r '.locations[]')
-echo $white"Download Url: $indi$downloadUrl"
+echo $whi"Download Url: $gre$downloadUrl"
 
 echo
-echo $white"A very large compressed file will be downloaded, size: $size_gb_format""Gb "
+echo $whi"A highly compressed file will expand to roughly four times its size upon extraction."
 echo
-echo $white"Please ensure that you've enough space to perform this operation."
+echo $whi"Please ensure that you've enough space to perform this operation."
 echo
-echo $white"Paste your Cardano Blockchain DB Path: "
+echo $whi"Paste your Cardano Blockchain DB Path: "
 echo
 read -r inputPath
 
 dbdir=$inputPath
 echo
 
-echo $white"Latest Mithril Snapshot $indi$digest"
-echo $white"will be downloaded under directory: $indi$dbdir"
-echo $indi
+echo $whi"Latest Mithril Snapshot $gre$digest"
+echo $whi"will be downloaded and deployed under directory: $gre$dbdir"
+echo $gre
 
 cd $dbdir
 
+start_time=$(date "+%s")
+
 wget -q --show-progress $downloadUrl
+echo
+echo $whi"Renaming $(ls $dbdir) to snapshot.tar.zst"
+mv mainnet-*.tar.zst snapshot.tar.zst
+echo
+echo $whi"Deploying Lastest Mainnet Snapshot: $gre$snapDigest"
+echo
+echo "Extracting snapshot.tar.zst"
+echo
+tar -xvf snapshot.tar.zst
+echo
+echo $whi"Deleting snapshot.tar.zst"
+rm snapshot.tar.zst
+echo
+echo $whi"Cardano Blockchain DB has been restored under: $gre$dbDir"
+echo $whi
+
+ls -l $dbDir
+echo
+
+end_time=$(date "+%s")
+elapsed=$(date -u -d @$((end_time - start_time)) +"%T")
+
+echo "Elapsed hh:mm:ss $elapsed"
