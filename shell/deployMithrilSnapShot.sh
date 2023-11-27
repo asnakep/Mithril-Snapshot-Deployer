933984ea053a1bbb9c10aab4efde1de70b8521d7
SNAKE (Cardano Stakepool) <53517506+asnakep@users.noreply.github.com>
2023-11-27 15:12:29 +0100

    Update deployMithrilSnapShot.sh

--- shell/deployMithrilSnapShot.sh
+++ shell/deployMithrilSnapShot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export white=`printf "\033[1;37m"`
-export indi=`printf "\033[1;35m"`
+export whi=`printf "\033[1;37m"`
+export gre=`printf "\033[1;36m"`
 
 tput clear
 
 echo
-echo $indi"Deploy Latest Mithril Mainnet Snapshot"
+echo $gre"Deploy Latest Mithril Mainnet Snapshot"
 echo
 
 ### snapshots json url
@@ -17,54 +17,52 @@ last_snapshot=$(curl -s $snapshots | jq -r .[0])
 
 # Get Snapshot data
 digest=$(echo $last_snapshot | jq -r '.digest')
-echo $white"Snapshot Digest: $indi$digest"
+echo $whi"Snapshot Digest: $gre$digest"
 
 network=$(echo $last_snapshot | jq -r '.beacon.network')
-echo $white"Network: $indi$network"
+echo $whi"Network: $gre$network"
 
 epoch=$(echo $last_snapshot | jq -r '.beacon.epoch')
-echo $white"Epoch: $indi$epoch"
+echo $whi"Epoch: $gre$epoch"
 
 immutable_file_number=$(echo $last_snapshot | jq -r '.beacon.immutable_file_number')
-echo $white"Immutable File Number: $indi$immutable_file_number"
+echo $whi"Immutable File Number: $gre$immutable_file_number"
 
 certificate_hash=$(echo $last_snapshot | jq -r '.certificate_hash')
-echo $white"Certificate Hash: $indi$certificate_hash"
+echo $whi"Certificate Hash: $gre$certificate_hash"
 
 size=$(echo $last_snapshot | jq -r '.size')
 size_gb=$((size / 1024 / 1024))
 size_gb_format=$(echo $size_gb | awk '{print substr($0, 1, length($0)-3) "." substr($0, length($0)-1)}')
-echo $white"Size: $indi$size_gb_format""Gb"
+echo $whi"Size: $gre$size_gb_format""Gb"
 
 created_at=$(echo $last_snapshot | jq -r '.created_at' | awk '{print substr($0, 1, length($0)-11)}')
-echo $white"Created At: $indi$created_at"
+echo $whi"Created At: $gre$created_at"
 
 compression_algorithm=$(echo $last_snapshot | jq -r '.compression_algorithm')
-echo $white"Compression Algorithm: $indi$compression_algorithm"
+echo $whi"Compression Algorithm: $gre$compression_algorithm"
 
 cardano_node_version=$(echo $last_snapshot | jq -r '.cardano_node_version')
-echo $white"Cardano Node Version: $indi$cardano_node_version"
+echo $whi"Cardano Node Version: $gre$cardano_node_version"
 
 downloadUrl=$(echo $last_snapshot | jq -r '.locations[]')
-echo $white"Download Url: $indi$downloadUrl"
+echo $whi"Download Url: $gre$downloadUrl"
 
 echo
-echo $white"A very large compressed file will be downloaded, take into account that once unarchived"
+echo $whi"A highly compressed file will expand to roughly four times its size upon extraction."
 echo
-echo $white"size will be about three times larger than compressed snapshot file."
+echo $whi"Please ensure that you've enough space to perform this operation."
 echo
-echo $white"Please ensure that you've enough space to perform this operation."
-echo
-echo $white"Paste your Cardano Blockchain DB Path: "
+echo $whi"Paste your Cardano Blockchain DB Path: "
 echo
 read -r inputPath
 
 dbdir=$inputPath
 echo
 
-echo $white"Latest Mithril Snapshot $indi$digest"
-echo $white"will be downloaded and deployed under directory: $indi$dbdir"
-echo $indi
+echo $whi"Latest Mithril Snapshot $gre$digest"
+echo $whi"will be downloaded and deployed under directory: $gre$dbdir"
+echo $gre
 
 cd $dbdir
 
@@ -72,19 +70,27 @@ start_time=$(date "+%s")
 
 wget -q --show-progress $downloadUrl
 echo
-echo $white"Renaming $(ls $dbdir) to snapshot.tar.zst"
+echo $whi"Renaming $(ls $dbdir) to snapshot.tar.zst"
 mv mainnet-*.tar.zst snapshot.tar.zst
 echo
-echo $white"Deploying Lastest Mainnet Snapshot: $indi$snapDigest"
+echo $whi"Deploying Lastest Mainnet Snapshot: $gre$snapDigest"
+echo
+echo $whi"Decompressing snapshot.tar.zst"
 echo
 zstd -d snapshot.tar.zst
+echo
+echo $whi"Deleting snapshot.tar.zst"
+rm snapshot.tar.zst
+echo
+echo "Extracting snapshot.tar"
+echo
 tar -xvf snapshot.tar
 echo
-echo $white"Deleting snapshot.tar.zst and snapshot.tar files"
-rm snapshot.tar.zst snapshot.tar
+echo $whi"Deleting snapshot.tar"
+rm snapshot.tar
 echo
-echo $white"Cardano Blockchain DB has been restored under: $indi$dbDir"
-echo $white
+echo $whi"Cardano Blockchain DB has been restored under: $gre$dbDir"
+echo $whi
 
 ls -l $dbDir
 echo
